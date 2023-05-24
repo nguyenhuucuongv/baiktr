234b2e1dee18942e1467ae8cb5101033de74c3b5
nguyenhuucuongv <128826244+nguyenhuucuongv@users.noreply.github.com>
2023-05-24 15:55:40 +0700

    Add files via upload

---
+++ GiaoDichDat.java
@@ -0,0 +1,34 @@
+package th.buoi4;
+
+public class GiaoDichDat extends GiaoDich {
+    private String loaiDat;
+ 
+    public GiaoDichDat() {
+        super();
+    }
+ 
+    public GiaoDichDat(String loaiDat) {
+        super();
+        this.loaiDat = loaiDat;
+    }
+ 
+    public String getLoaiDat() {
+        return loaiDat;
+    }
+ 
+    public void setLoaiDat(String loaiDat) {
+        this.loaiDat = loaiDat;
+    }
+     
+    @Override
+    public void nhap() {
+        super.nhap();
+        System.out.print("Nhập loại đất (A/B/C): ");
+        loaiDat = scanner.nextLine();
+    }
+ 
+    @Override
+    public String toString() {
+        return super.toString() + ", loại đất: " + this.loaiDat;
+    }
+}
